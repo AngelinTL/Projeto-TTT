6fda9adbcdf0ab9460cab18aa3a1137a0bd39bff
AngelinTL <angelo.lunardini@hotmail.com>
2025-07-31 20:12:30 -0300

    first commit

---
+++ login-page/src/app/services/login.service.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginResponse } from '../types/login-response.type';
+import { tap } from 'rxjs';
+import { environment } from '../enviroments/enviroment.prod';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+  apiUrl: string = environment.apiUrl;
+
+  constructor(private httpClient: HttpClient) { }
+  
+  login(email: string, password: string) { 
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem('auth-token', value.token)
+        sessionStorage.setItem('username', value.name)
+      })
+    )
+  }
+
+  signup(name: string, email: string, password: string) { 
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem('auth-token', value.token)
+        sessionStorage.setItem('username', value.name)
+      })
+    )
+  }
+}
